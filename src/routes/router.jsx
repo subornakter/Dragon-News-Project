5ae275e97cdec4bc99b7b00a1f5da9734211fa98
subornakter <subornakter01@gmail.com>
2025-10-19 10:56:05 +0600

    firebase setup

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,6 +2,11 @@ import { createBrowserRouter } from "react-router";
 import HomeLayout from "../layouts/HomeLayout";
 import CategoryNews from "../pages/CategoryNews";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import AuthLayout from "../layouts/AuthLayout";
+import NewsDetails from "../pages/NewsDetails";
+import PrivateRoute from "../provider/PrivateRoute";
 
 const router = createBrowserRouter([
   {
@@ -19,7 +24,33 @@ const router = createBrowserRouter([
       },
     ],
   },
-
+  {
+    path: "/auth",
+    element: <AuthLayout></AuthLayout>,
+    children: [
+      {
+        path: "/auth/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/auth/register",
+        element: <Register></Register>,
+      },
+    ],
+  },
+  {
+    path: "/news-details/:id",
+    element: (
+      <PrivateRoute>
+        <NewsDetails></NewsDetails>
+      </PrivateRoute>
+    ),
+    loader: () => fetch("/news.json"),
+  },
+  {
+    path: "/*",
+    element: <h2>Error404</h2>,
+  },
 ]);
 
 export default router;
